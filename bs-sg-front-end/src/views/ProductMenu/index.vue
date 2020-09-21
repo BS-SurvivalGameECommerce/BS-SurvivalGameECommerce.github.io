f6b4ae825fca22fc1082b5938fe70bded6688c39
Hank <h50614ank@gmail.com>
2020-09-21 20:15:45 +0800

    MenuApplycomponent

--- bs-sg-front-end/src/views/ProductMenu/index.vue
+++ bs-sg-front-end/src/views/ProductMenu/index.vue
@@ -1,16 +1,20 @@
 <script>
 import axios from "axios";
+import ProductCardGroup from '../../components/ProductCardGroup/index.vue'
 
 var domain = "https://localhost:5001/";
-var api=domain+"Product/Menu"
+var api = domain + "Product/Menu";
 var product = [];
 var CatagoryName = [];
 
 export default {
   name: "Menu",
+  components: {
+    ProductCardGroup,
+  },
   data() {
     return {
-      product: product,
+      Products: product,
       CatagoryName: CatagoryName,
     };
   },
@@ -20,10 +24,11 @@ export default {
       method: "Get",
     }).then((res) => {
       let response = res.data;
-      console.log(response);
+      // console.log(response);
       response.data.forEach((item) => {
         product.push(item);
       });
+      console.log(product)
       response.data.forEach((item) => {
         if (CatagoryName.indexOf(item.catagoryName) == -1) {
           CatagoryName.push(item.catagoryName);
@@ -32,18 +37,17 @@ export default {
     });
   },
   methods: {
-    Addcart:function(x){
-      alert("Add to Cart")
-       this.$store.dispatch("addToCart", { pid: x, quantity: 1 });
-    },
     pselect: function (x) {
-      this._data.product = product.filter(function (item) {
+      this._data.Products = product.filter(function (item) {
         return item.catagoryName == x;
       });
     },
     reset: function () {
-      this._data.product = product;
+      this._data.Products = product;
     },
+  },
+  updated: function(){
+    this.InitAnime();
   }
 };
 
