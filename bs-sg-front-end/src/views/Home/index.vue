e9613fdce8bbfc38e03a97407173559cad2b2975
frank-fn10502 <frank08.chen@gmail.com>
2020-09-21 14:36:58 +0800

    refact product card group style

--- bs-sg-front-end/src/views/Home/index.vue
+++ bs-sg-front-end/src/views/Home/index.vue
@@ -18,8 +18,7 @@ export default {
   mounted: function () {
     let self = this;
     axios({
-      url:
-        "https://sgwebapi-core.azurewebsites.net/Product/NewestSimpleProduct/8",
+      url:`${this.$store.state.domain}Product/NewestSimpleProduct/8`,
       method: "Get",
     }).then((res) => {
       let response = res.data;
@@ -38,4 +37,4 @@ export default {
 </script>
 
 <template src="./template.html"></template>
-<style src="./style.css" scoped></style>
+<style lang="scss" src="./style.scss" scoped></style>
